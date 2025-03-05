7a1490952e3ee94ba5b051e15bb9aa3e04884b0e
Prad N <prad@didao.xyz>
2025-03-05 17:44:49 -0500

    refactor: improve installation process and cleanup

--- install.sh
+++ install.sh
@@ -3,18 +3,18 @@
 set -e
 
 CLEAN=0
-GIT_URL="https://github.com/prnk28/wave.git"
 WAVE_DIR="$HOME/.local/share/wave"
 
 function freshClone() {
     rm -rf "$WAVE_DIR"
-    git clone "$GIT_URL" "$WAVE_DIR"
+    cd ~/.local/share
+    gh repo clone prnk28/wave
     cd "$WAVE_DIR"
     chmod +x "./bin/wave"
     mv ./bin/wave ~/.local/bin
 }
 
-if [ CLEAN -eq 1 ]; then
+if [ CLEAN == 1 ]; then
     freshClone
 else
     cd "$WAVE_DIR"
